b592b89e764bebd8a5f5c3180d9b9dbdfe2a1205
Prince Bhagat <hrprem16@gmail.com>
2024-02-08 03:47:04 +0530

    [Prince] Add: First Commit

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Address_Book_System
+{
+    class Program
+    {
+        static void Main()
+        {
+            Console.WriteLine("Welcome to Address Book");
+        }
+    }
+}
