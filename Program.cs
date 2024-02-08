3dd0e429197785079f5f45740051342b89f44633
Prince Bhagat <hrprem16@gmail.com>
2024-02-08 06:05:43 +0530

    [Prince]Add: features to add multiple contacts

--- Program.cs
+++ Program.cs
@@ -12,9 +12,44 @@ namespace Address_Book_System
             Console.WriteLine("Welcome to Address Book");
 
             AddressBook ad1 = new AddressBook();
-            ad1.addContact();
-            ad1.display();
-            ad1.editContact("abc");
+            bool isrunning = true;
+            while (isrunning)
+            {
+                Console.WriteLine("Enter the option to perform");
+                Console.WriteLine("1.Add Contact");
+                Console.WriteLine("2.Display Contact");
+                Console.WriteLine("3.Edit Contact");
+                Console.WriteLine("4.Delete Contact");
+                Console.WriteLine("5.Exit");
+
+
+                int input1 = Convert.ToInt32(Console.ReadLine());
+                switch (input1)
+                {
+                    case 1:
+                        ad1.addContact();
+                        break;
+                    case 2:
+                        ad1.display();
+                        break;
+                    case 3:
+                        Console.WriteLine("Enter the  first name of the person you want to edit ");
+                        string name = Console.ReadLine();
+                        ad1.editContact(name);
+                        break;
+                    case 4:
+                        Console.WriteLine("Enter the  first name of the person you want to delete ");
+                        string nam = Console.ReadLine();
+                        ad1.deleteContact(nam);
+                        break;
+                    case 5:
+                        isrunning = false;
+                        break;
+
+                }
+
+            }
+
         }
     }
 }
